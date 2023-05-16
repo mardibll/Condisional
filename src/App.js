550961a74ea79d56e7b3fb208ab151bbfa2d2313
mardi_buulolo123 <mardimbuulolo@gmail.com>
2023-05-16 16:59:30 +0700

    commit latihan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const nilai = 50;
+
+  const condisional = nilai > 60 ? "Jawabn Benar" : "Jawaban Salah";
+  console.log(condisional);
+
+  const condisionalDan = nilai > 60 && "hallo";
+  console.log(condisionalDan);
+
+  const condisionalOr = nilai > 60 || "condisionalor";
+  console.log(condisionalOr);
+
+  const datasiswa = [
+    {
+      namaWali: "Sri Mulyani Hartini",
+      kelas: "x",
+      jumlahSiswa: 50,
+    },
+    {
+      namaWali: "Yohanes Ferdi Oloan",
+      kelas: "xl",
+      jumlahSiswa: 55,
+    },
+    {
+      namaWali: "Sulaiman",
+      kelas: "xll",
+      jumlahSiswa: 42,
+    },
+  ];
+  const listSiswa = datasiswa.map((item, index) => item);
+  console.log(listSiswa);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{}}>
+      <table className="table" style={{}}>
+        <thead>
+          <tr>
+            <th scope="col">Wali kelas</th>
+            <th scope="col">Kelas</th>
+            <th scope="col">jumlah Siswa</th>
+          </tr>
+        </thead>
+        {datasiswa.map((item, index) => {
+          return (
+            <tbody key={index}>
+              <tr style={{ borderWidth: 3 }}>
+                <td>{item.namaWali}</td>
+                <td>{item.kelas}</td>
+                <td>{item.jumlahSiswa + 10}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </div>
   );
 }
